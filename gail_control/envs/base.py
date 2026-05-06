147e8339173a347d7475908b1e4faefb9e7ce28d
duankuxiao <duankuxiao@gmail.com>
2026-05-06 16:29:38 +0800

    Reorganize control experiment codebase

--- gail_control/envs/base.py
+++ gail_control/envs/base.py
@@ -240,9 +240,9 @@ class BaseEnv(gym.Env):
         path = self.train_data_path if flag == "train" else self.test_data_path
         inputs = pd.read_csv(path, index_col=0).iloc[:46081, :]
         inputs.index = pd.to_datetime(inputs.index)
-        inputs["Ec_pv"] = inputs["PV"] * 1.4
-        inputs["Ec_other"] = inputs["Ec_demand"] * 0.5
-        inputs["phi_i"] = inputs["phi_i"] + 0.5 * inputs["phi_i"] * (inputs["home"] == 1)
+        inputs["Ec_pv"] = inputs["PV"]   # * 1.4
+        inputs["Ec_other"] = inputs["Ec_demand"]   # * 0.5
+        inputs["phi_i"] = inputs["phi_i"]   # + 0.5 * inputs["phi_i"] * (inputs["home"] == 1)
         return inputs
 
     def _denormalize_act(self, action: np.ndarray) -> np.ndarray:
@@ -549,12 +549,6 @@ class BaseEnv(gym.Env):
         res = res.copy()
         res["L"] = res["L"] * 0.8
 
-        if self.price == "fix":
-            sr_baseline, cost_baseline = 0.8662, 22235
-        elif self.price == "normal":
-            sr_baseline, cost_baseline = 0.8662, 22878
-        else:
-            sr_baseline, cost_baseline = 0.8662, 24377
         if self.baseline_sr_reference is not None:
             sr_baseline = self.baseline_sr_reference
         if self.baseline_cost_reference is not None:
